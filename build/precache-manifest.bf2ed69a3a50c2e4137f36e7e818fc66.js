d7dc78dfaaa0dd7981fb7301dd795f90617fee07
Abhishek Gautam <abhishekgautm243@gmail.com>
2020-07-04 20:12:06 +0530

    commit

---
+++ build/precache-manifest.bf2ed69a3a50c2e4137f36e7e818fc66.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a6413077d547f43d655b9cbea97ab26",
+    "url": "/index.html"
+  },
+  {
+    "revision": "26c30fa5d97fe7403331",
+    "url": "/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "24c7a75f5eacdf7ca6e6",
+    "url": "/static/css/main.4ede5ffd.chunk.css"
+  },
+  {
+    "revision": "26c30fa5d97fe7403331",
+    "url": "/static/js/2.cf64f1e9.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.cf64f1e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24c7a75f5eacdf7ca6e6",
+    "url": "/static/js/main.0901d1cd.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "d3b9be2ae787177cbcbbe03c0eef562f",
+    "url": "/static/media/architecture.d3b9be2a.jpg"
+  }
+]);
